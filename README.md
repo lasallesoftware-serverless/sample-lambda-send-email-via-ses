c3c8c1fe523b8af79d9db95c981ec74bd743ebd4
Bob Bloom <bob.bloom@lasallesoftware.ca>
2021-12-21 22:30:45 -0500

    Initial commit. #1

---
+++ README.md
@@ -0,0 +1,63 @@
+<h1 align="center">PHP Serverless Project's Sample Lambda: Send email via SES</h1>
+
+<p align="center">
+    <strong>Sample PHP Lambda function to send email via SES, made with the Serverless Framework and Bref</strong>
+</p>
+
+<hr>
+
+<br>
+
+This repo uses:
+- Serverless Framework v2
+- Bref's PHP plugin for the Serverless Framework
+- Bref's PHP runtime for Lambda
+
+<br>
+
+Please note that you have to set up email addresses manually in the SES dashboard.
+
+<br>
+
+See what this PHP Lambda function looks like in your AWS console. Check out these dashboards:
+- Lambda
+- S3
+- IAM
+- CloudFormation
+- API Gateway
+- SES
+
+<br>
+<hr>
+
+## Links
+<br>
+
+- [Installation][]
+- [Caveats][]
+- [Changelog][]
+
+<br>
+<hr>
+
+## Copyright and License
+<br>
+
+lasallesoftware-serverless/sample-lambda-send-email-via-ses is copyright © The South LaSalle Trading Corporation and
+licensed for use under the MIT License (MIT).
+
+Please see [LICENSE][] for more information.
+
+As reference, there is a wonderful blog post called [The MIT License, Line by Line -- 171 words every programmer should understand](https://writing.kemitchell.com/2016/09/21/MIT-License-Line-by-Line.html).
+
+<br><br>
+<hr>
+<br>
+
+>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+[installation]: https://github.com/lasallesoftware-serverless/sample-lambda-hello-world/blob/master/INSTALLATION.md
+[caveats]: https://github.com/lasallesoftware-serverless/sample-lambda-hello-world/blob/master/CAVEATS.md
+[changelog]: https://github.com/lasallesoftware-serverless/sample-lambda-send-email-via-ses/blob/master/CHANGELOG.md
+[license]: https://github.com/lasallesoftware-serverless/sample-lambda-hello-world/blob/master/LICENSE.md
---
+++ src/Handler.php
@@ -0,0 +1,107 @@
+<?php
+
+/**
+ * This file is part of the Lasalle Software Serverless Sending email via SES sample PHP Lambda function.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright  (c) 2021-2022 The South LaSalle Trading Corporation
+ * @license    http://opensource.org/licenses/MIT
+ * @author     Bob Bloom
+ * @email      bob.bloom@lasallesoftware.ca
+ * @link       https://phpserverlessproject.com
+ * @link       https://packagist.org/packages/lasallesoftware-serverless/sample-lambda-send-email-via-ses
+ * @link       https://github.com/lasallesoftware-serverless/sample-lambda-send-email-via-ses
+ *
+ */
+
+require './../vendor/autoload.php';
+
+
+// Set the vars
+$region = 'us-east-1';   // same region that you specified in the serverless.yml
+$version = '2010-12-01';
+
+// This email address must be verified with Amazon SES, regardless of your SES being in the sandbox.
+$sender_email = 'sender@email.com';
+
+// IMPORTANT: If your account is still in the sandbox, these addresses must be verified.
+// https://docs.aws.amazon.com/ses/latest/DeveloperGuide/request-production-access.html
+$recipient_emails = ['recipient1@email.com', 'recipient2@email.com'];
+
+// Email parameters
+$subject = 'Email subject line';
+$plaintext_body = 'Email body in plain text format.' ;
+$html_body =  '<h1>Email body in HTML format</h1>'.
+              '<p>This email was sent via AWS SES.</p>';
+              '<p>This email was sent from your sample-lambda-send-email-via-ses PHP Lambda function.</p>';
+$char_set = 'UTF-8';
+
+
+// Create the SES client
+if (isset($_SERVER['LAMBDA_TASK_ROOT'])) {
+
+    // For Lambda
+    $SesClient = new \Aws\Ses\SesClient([
+        'region'  => $region,
+        'version' => $version,
+    ]);
+
+} else {
+
+    // For your local development
+    $SesClient = new \Aws\Ses\SesClient([
+        'profile' => 'default',
+        'region'  => $region,
+        'version' => $version,
+    ]);
+
+}
+
+
+// Send the email
+// https://docs.aws.amazon.com/aws-sdk-php/v3/api/api-sesv2-2019-09-27.html#sendemail
+try {
+    $result =  $SesClient->sendEmail([
+        'Destination' => [
+            'ToAddresses' => $recipient_emails,
+        ],
+        'ReplyToAddresses' => [$sender_email],
+        'Source' => $sender_email,
+        'Message' => [
+          'Body' => [
+              'Html' => [
+                  'Charset' => $char_set,
+                  'Data' => $html_body,
+              ],
+              'Text' => [
+                  'Charset' => $char_set,
+                  'Data' => $plaintext_body,
+              ],
+          ],
+          'Subject' => [
+              'Charset' => $char_set,
+              'Data' => $subject,
+          ],
+        ],
+    ]);
+    var_dump($result);
+
+} catch (\Aws\Exception\AwsException $e) {
+    
+    if (isset($_SERVER['LAMBDA_TASK_ROOT'])) {
+        
+        // for Lambda, log the error message
+        error_log($e->getMessage());
+
+    } else {
+
+         // for your local development, output error message
+        var_dump($e->getMessage());
+    }
+}
